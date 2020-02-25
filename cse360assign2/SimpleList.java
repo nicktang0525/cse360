aad043f9df92b373a13752e847e29742b32c703c
Nick Tang <ntang0525@gmail.com>
2020-02-25 14:30:58 -0700

    create a folder and added simpleList.java file

---
+++ cse360assign2/SimpleList.java
@@ -0,0 +1,13 @@
+// Author Name: Nicholas Tang
+// Class ID: 212
+// Assignment 1
+
+public class SimpleList {
+    private int[] list; //Class Members
+    private int count;
+
+    public SimpleList() {
+        list = new int[10];
+        count = 0;
+    }
+}
